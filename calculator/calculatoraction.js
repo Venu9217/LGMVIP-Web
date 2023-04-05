8d2fe6c56d85d43894e4e0376125ab47a034d58e
Venu9217 <93443457+Venu9217@users.noreply.github.com>
2023-04-05 17:07:21 +0530

    Update calculatoraction.js

--- calculator/calculatoraction.js
+++ calculator/calculatoraction.js
@@ -1,10 +1,16 @@
 function dis(val){
     document.getElementById("res").value+=val;
     }
-    function solve(){
+    function solve(){ 
+    try{ 
     let x=document.getElementById("res").value;
     let y=eval(x);
     document.getElementById("res").value=y;
+    }
+    catch{
+        //alert("invalid");
+        document.getElementById("res").value='invalid';
+    }     
     }
     function clr(){
     document.getElementById("res").value="";
